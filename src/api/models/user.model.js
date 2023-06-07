0ca7810ef06de9f9c0f0496be5ca5d41359eddef
Aitor Gutierrez <gutierrezizquierdoaitor@gmail.com>
2023-06-07 17:57:11 +0200

    update model user

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -33,7 +33,7 @@ const UserSchema = new mongoose.Schema(
     rol: {
       type: String,
       enum: ['admin', 'user'],
-      required: true,
+      required: false,
     },
     confirmationCode: {
       type: Number,
